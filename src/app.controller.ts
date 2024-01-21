8504b488db7fff42bd3ad5c52811035c42ff2e59
YALNEZAMI <yaseralnezami@gmail.com>
2024-01-21 19:38:11 +0100

    new version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseInterceptors,
+  Post,
+  UploadedFiles,
+  Body,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import * as fs from 'fs';
 import { AppService } from './app.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', 10, {
+      storage: diskStorage({
+        destination: './src/files',
+        filename: (req, file, callback) => {
+          return callback(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  create(@Body() object: any, @UploadedFiles() files: any[]) {
+    const file = files[0];
+    console.log(file);
+
+    return {
+      message: 'success',
+    };
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
   @Get('file')
-  contentFiles(): string {
-    return this.appService.contentFiles();
+  async contentFiles() {
+    return await this.appService.contentFiles();
+  }
+  @Get('files/:fileId')
+  sendFile(@Param('fileId') fileId: string, @Res() res: Response) {
+    fs.access('/files/' + fileId, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.download('./src/files/test.txt', 'test.txt');
+        res.sendFile('/files/test.txt', { root: 'src' });
+        // Handle the case where the file does not exist
+      } else {
+        res.download('./src/files/' + fileId, fileId);
+        res.sendFile('/files/' + fileId, { root: 'src' });
+        // Handle the case where the file exists
+      }
+    });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,57 +4,21 @@ import * as path from 'path';
 @Injectable()
 export class AppService {
   private cf: string = 'yes';
-  constructor() {
-    const folderPath = './src/files';
-    const filePath = './src/files/result.txt';
-    // if (fs.existsSync(filePath)) {
-    //   fs.unlinkSync(filePath);
-    // }
-    // Use fs.truncate to empty the file
-    fs.truncate(filePath, 0, (err) => {
-      if (err) {
-        console.error('Error emptying file:', err);
-        return;
-      }
-      console.log('File emptied successfully');
-    });
-    // setTimeout(() => {
-    // Read files in the folder
-    fs.readdir(folderPath, (err, files) => {
-      if (err) {
-        console.error('Error reading folder:', err);
-        return;
-      }
-
-      const concatenatedContent = [];
-
-      // Read and concatenate each file
-      files.forEach((file) => {
-        const filePath = path.join(folderPath, file);
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        concatenatedContent.push(fileContent);
-      });
-
-      // Write the concatenated content to the output file
-      fs.writeFileSync(filePath, concatenatedContent.join('\n'), 'utf-8');
-      console.log('Files have been concatenated and written to', filePath);
-    });
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        console.error('Error reading file:', err);
-        return 'Error reading file';
-      } else {
-        this.cf = data;
-      }
-    });
-    //     ,5000;
-    // });
-  }
+  constructor() {}
   getHello(): string {
     return 'Hello World!';
   }
 
-  contentFiles(): any {
-    return this.cf;
+  async contentFiles() {
+    const folderPath = './src/files';
+    const res: { name: string; content: string }[] = [];
+    const files = fs.readdirSync(folderPath);
+
+    for (let i = 0; i < files.length; i++) {
+      const filePath = path.join(folderPath, files[i]);
+      const content = fs.readFileSync(filePath, 'utf-8');
+      res.push({ name: files[i], content });
+    }
+    return res;
   }
 }
